15c983108b96f649cdf9f52b74fc5885bc29f63b
Ywoosang <opellong13@gmail.com>
2021-12-04 18:26:27 +0900

    Add auth service #11

---
+++ src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import AuthDao from '../daos/auth.dao'
+import RegisterUserDto from '../dtos/user.dto'; 
+import HttpException from '../exceptions/http.exception';
+import  * as bcrypt from 'bcrypt';
+
+class AuthService {
+    private authDao = new AuthDao(); 
+
+    public registerUser = async (user : RegisterUserDto) => {
+        console.log(user);
+        
+        const exUser = await this.authDao.getUserByEmail(user.email);
+        if (exUser) {
+            console.log('이미 사용자가 존재합니다.');
+            throw new HttpException(400,'이미 사용자가 존재합니다.');
+        }
+        const hash = await bcrypt.hash(user.password,12)
+        await this.authDao.postUser({
+             email: user.email,
+             name: user.name,
+             password: hash,
+        });
+    }
+}
+
+export default AuthService;
