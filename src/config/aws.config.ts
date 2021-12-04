fc7d722beeb82d1027149256bef3b816eac9722b
Ywoosang <opellong13@gmail.com>
2021-12-04 18:03:06 +0900

    Add aws config

---
+++ src/config/aws.config.ts
@@ -0,0 +1,12 @@
+import * as AWS from 'aws-sdk'; 
+import * as dotenv from 'dotenv'; 
+dotenv.config(); 
+ 
+AWS.config.update({
+    apiVersion: "2010-12-01",
+    accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey:process.env.AWS_SECRET_KEY,
+    region: 'ap-northeast-2',
+});
+
+export default AWS;
