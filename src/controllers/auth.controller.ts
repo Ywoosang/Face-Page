186869f1f90700bc14e0c13f18b89c95062b15bc
Ywoosang <opellong13@gmail.com>
2021-12-06 01:38:50 +0900

    Add auth controller #19

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,6 +5,7 @@ import AuthService from '../services/auth.service';
 import RegisterUserDto from '../dtos/user.dto';
 import * as passport from 'passport';
 import HttpException from '../exceptions/http.exception';
+import { isLoggedIn, isNotLoggedIn } from '../middlewares/auth.middleware';
 
 dotenv.config();
 
@@ -18,13 +19,30 @@ class AuthController implements Controller {
     }
 
     private initializeRoutes() {
+        // 인증여부 확인
+        this.router.get(`${this.path}`,this.isAuthenticated)
         // 회원가입 
-        this.router.post(`${this.path}/signup`, this.signUp);
+        this.router.post(`${this.path}/signup`,isNotLoggedIn, this.signUp);
         // 로그인
-        this.router.post(`${this.path}/signin`, this.siginIn);
+        this.router.post(`${this.path}/signin`,isNotLoggedIn, this.siginIn);
         // 로그아웃
-        this.router.get(`${this.path}/signout`, this.signOut);
-
+        this.router.get(`${this.path}/signout`,isLoggedIn, this.signOut);
+        // 카카오 로그인
+        this.router.get(`${this.path}/kakao`,isNotLoggedIn, passport.authenticate('kakao'));
+        // 카카오 로그인 콜백
+        this.router.get(`${this.path}/kakao/callback`, passport.authenticate('kakao'),this.snsSignIn);
+        // 구글 로그인 
+        this.router.get(`${this.path}/google`,isNotLoggedIn, passport.authenticate('google',{ scope: ['profile', 'email'] }));
+        // 구글 로그인 콜백
+        this.router.get(`${this.path}/google/callback`, passport.authenticate('google'),this.snsSignIn);
+        // 네이버 로그인
+        this.router.get(`${this.path}/naver`,isNotLoggedIn, passport.authenticate('naver'));
+        // 네이버 로그인 콜백
+        this.router.get(`${this.path}/naver/callback`, passport.authenticate('naver'), this.snsSignIn);
+        // 깃허브 로그인
+        this.router.get(`${this.path}/github`,isNotLoggedIn, passport.authenticate('github'));
+        // 깃허브 로그인 콜백
+        this.router.get(`${this.path}/github/callback`, passport.authenticate('github'),this.snsSignIn);
     }
     // https://norux.me/61
     private  signUp = async(req,res,next) => {
@@ -56,10 +74,41 @@ class AuthController implements Controller {
         })(req,res,next)
     }
 
+    private isAuthenticated = (req,res,next) => {
+        try{
+            console.log(req.isAuthenticated())
+            if(req.isAuthenticated()){
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(401);
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+
     private signOut = async(req,res,next) => {
-        req.logout();
-        req.session.destory();
-        res.sendStatus(200);
+        try{
+            req.logout();
+            if(req.session){
+                req.session.destroy((error)=>{
+                   if(error){
+                     console.log(error);
+                   }
+                });
+             }
+            return res.sendStatus(200);
+        }catch(error){
+            next(error);
+        }
+    }
+
+    private snsSignIn = async(req, res,next) => {
+        try{
+            res.redirect('http://localhost:8080'); 
+        }catch(error){
+            next(error); 
+        }
     }
 };
 
