6a3d702c1c38701416b1e9b3f8d57437d0de6d51
Ywoosang <opellong13@gmail.com>
2021-12-06 01:44:15 +0900

    Add passport sns oauth strategies #19

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,10 +1,15 @@
 import * as passport from 'passport';
 import local from './localStrategy';
+import kakao from './kakaoStrategy';
+import google from './googleStrategy';
+import naver from './naverStrategy';
+import github from './githubStrategy'; 
 import AuthDao from '../daos/auth.dao';
+ 
 
-const authDao = new AuthDao();
 
 export default ()=>{
+    const authDao = new AuthDao();
     passport.serializeUser((user: any,done) => {
         done(null, user.id); // 세션에 user 의 id 만 저장
     });
@@ -12,10 +17,17 @@ export default ()=>{
     passport.deserializeUser(async(id: number,done) => {
         try{
             const user = await authDao.getUserById(id);
+            // req.user 로 설정
+            console.log('실행',user)
             done(null,user);
         } catch(error){
             done(error);
         }
     })
-
+    local();
+    kakao();
+    google();
+    naver();
+    github();
 }
+ 
