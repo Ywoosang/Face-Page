baa1f3518d1dd615434bacd94666a6c16dab019a
Ywoosang <opellong13@gmail.com>
2021-12-06 01:41:09 +0900

    Add kakao oauth #19

---
+++ src/passport/kakaoStrategy.ts
@@ -0,0 +1,32 @@
+import * as passport from 'passport'
+import * as passportKakao  from 'passport-kakao';
+import * as dotenv from 'dotenv'
+import AuthDao from '../daos/auth.dao';
+dotenv.config();
+
+const KakaoStrategy = passportKakao.Strategy
+export default () => {
+   const authDao = new AuthDao();
+   passport.use(new KakaoStrategy({
+       clientID : process.env.KAKAO_CLIENT_ID,
+       clientSecret: process.env.KAKAO_CLIENT_SECRET,
+       callbackURL : '/api/auth/kakao/callback',
+   },async (accessToken,refreshToken,profile,done)=>{
+       try {
+           const exUser = await authDao.getUserBySnsInfo(profile.id,'kakao'); 
+           if(exUser){
+               done(null,exUser);
+           }else {
+               const newUser = await authDao.postUser({
+                   email : profile._json.kakao_account.email,
+                   name : profile.displayName,
+                   snsId : profile.id,
+                   provider : 'kakao'
+               });
+               done(null,newUser); 
+           }
+       }catch(error) {
+           done(error);
+       }
+   }))
+}
