ebe22ea004f49cddaf914f9182b4ad9da7f6f2e6
Ywoosang <opellong13@gmail.com>
2021-12-07 06:17:48 +0900

    Remove unneeded files

--- src/interfaces/typeorm.config.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectionOptions } from "typeorm";
-
-interface TypeormConfig {
-    development: ConnectionOptions;
-    production: ConnectionOptions; 
-}
-
-export default TypeormConfig;
